dd498da5bb32213bc4c96894d8a08af337d1c454
Ian Lehmann <ianlehman11@hotmail.com>
2023-05-14 20:41:39 -0300

    Base de conocimiento completada

--- src/main/java/se/BaseDeConocimiento.java
+++ src/main/java/se/BaseDeConocimiento.java
@@ -157,6 +157,139 @@ public class BaseDeConocimiento {
     }
     
     private static void diagnosticoCOVID(RuleInferenceEngine rie){
+
+        Rule rule = new Rule("COVID_Regla_1");
+        rule.addAntecedent(new EqualsClause(Hechos.fiebre, "si"));
+        rule.addAntecedent(new EqualsClause(Hechos.tos, "si"));
+        rule.addAntecedent(new EqualsClause(Hechos.dificultadRespirar, "si"));
+        rule.addAntecedent(new EqualsClause(Hechos.fatiga, "si"));
+        rule.setConsequent(new EqualsClause(Hechos.COVID, "si"));
+        rie.addRule(rule);
+
+        Rule rule = new Rule("COVID_Regla_2");
+        rule.addAntecedent(new EqualsClause(Hechos.fiebre, "si"));
+        rule.addAntecedent(new EqualsClause(Hechos.dolorGarganta, "si"));
+        rule.addAntecedent(new OrClause(
+                new EqualsClause(Hechos.perdidaOlfato, "si"),
+                new EqualsClause(Hechos.perdidaGusto, "si")
+        ));
+        rule.setConsequent(new EqualsClause(Hechos.COVID, "si"));
+        rie.addRule(rule);
+
+        Rule rule = new Rule("COVID_Regla_3");
+        rule.addAntecedent(new EqualsClause(Hechos.diarrea, "si"));
+        rule.addAntecedent(new EqualsClause(Hechos.dolorCabeza, "si"));
+        rule.addAntecedent(new EqualsClause(Hechos.congestionNasal, "si"));
+        rule.setConsequent(new EqualsClause(Hechos.COVID, "si"));
+        rie.addRule(rule);
+
+        Rule rule = new Rule("COVID_Regla_4");
+        rule.addAntecedent(new EqualsClause(Hechos.contactoCOVID, "si"));
+        rule.addAntecedent(new EqualsClause(Hechos.fiebre, "si"));
+        rule.addAntecedent(new EqualsClause(Hechos.tos, "si"));
+        rule.addAntecedent(new EqualsClause(Hechos.congestionNasal, "si"));
+        rule.setConsequent(new EqualsClause(Hechos.COVID, "si"));
+        rie.addRule(rule);
+        
+        Rule rule = new Rule("COVID_Regla_5");
+        rule.addAntecedent(new EqualsClause(Hechos.fiebre, "si"));
+        rule.addAntecedent(new EqualsClause(Hechos.escalofrios, "si"));
+        rule.addAntecedent(new EqualsClause(Hechos.dolorGarganta, "si"));
+        rule.addAntecedent(new OrClause(
+                new EqualsClause(Hechos.perdidaOlfato, "si"),
+                new EqualsClause(Hechos.perdidaGusto, "si")
+        ));
+        rule.setConsequent(new EqualsClause(Hechos.COVID, "si"));
+        rie.addRule(rule);
+        
+        Rule rule = new Rule("COVID_Regla_6");
+        rule.addAntecedent(new EqualsClause(Hechos.fiebre, "si"));
+        rule.addAntecedent(new EqualsClause(Hechos.tos, "si"));
+        rule.addAntecedent(new EqualsClause(Hechos.dolorPecho, "si"));
+        rule.addAntecedent(new EqualsClause(Hechos.dificultadRespirar, "si"));
+        rule.setConsequent(new EqualsClause(Hechos.COVID, "si"));
+        rie.addRule(rule);
+        
+        Rule rule = new Rule("COVID_Regla_7");
+        rule.addAntecedent(new EqualsClause(Hechos.fiebre, "si"));
+        rule.addAntecedent(new EqualsClause(Hechos.dolorCabeza, "si"));
+        rule.addAntecedent(new EqualsClause(Hechos.congestionNasal, "si"));
+        rule.addAntecedent(new EqualsClause(Hechos.debilidadGeneralziada, "si"));
+        rule.setConsequent(new EqualsClause(Hechos.COVID, "si"));
+        rie.addRule(rule);
+        
+        Rule rule = new Rule("COVID_Regla_8");
+        rule.addAntecedent(new EqualsClause(Hechos.fiebre, "si"));
+        rule.addAntecedent(new EqualsClause(Hechos.tos, "si"));
+        rule.addAntecedent(new EqualsClause(Hechos.dificultadRespirar, "si"));
+        rule.addAntecedent(new OrClause(
+                new EqualsClause(Hechos.perdidaOlfato, "si"),
+                new EqualsClause(Hechos.perdidaGusto, "si")
+        ));
+        rule.setConsequent(new EqualsClause(Hechos.COVID, "si"));
+        rie.addRule(rule);
+        
+        Rule rule = new Rule("COVID_Regla_9");
+        rule.addAntecedent(new EqualsClause(Hechos.fiebre, "si"));
+        rule.addAntecedent(new EqualsClause(Hechos.dolorGarganta, "si"));
+        rule.addAntecedent(new EqualsClause(Hechos.congestionNasal, "si"));
+        rule.addAntecedent(new EqualsClause(Hechos.diarrea, "si"));
+        rule.setConsequent(new EqualsClause(Hechos.COVID, "si"));
+        rie.addRule(rule);
+        
+        Rule rule = new Rule("COVID_Regla_10");
+        rule.addAntecedent(new EqualsClause(Hechos.fiebre, "si"));
+        rule.addAntecedent(new EqualsClause(Hechos.dolorCabeza, "si"));
+        rule.addAntecedent(new EqualsClause(Hechos.dolorGarganta, "si"));
+        rule.addAntecedent(new EqualsClause(Hechos.fatiga, "si"));
+        rule.setConsequent(new EqualsClause(Hechos.COVID, "si"));
+        rie.addRule(rule);
+        
+        Rule rule = new Rule("COVID_Regla_11");
+        rule.addAntecedent(new EqualsClause(Hechos.fiebre, "si"));
+        rule.addAntecedent(new EqualsClause(Hechos.tos, "si"));
+        rule.addAntecedent(new EqualsClause(Hechos.dificultadRespirar, "si"));
+        rule.addAntecedent(new EqualsClause(Hechos.dolorCabeza, "si"));
+        rule.setConsequent(new EqualsClause(Hechos.COVID, "si"));
+        rie.addRule(rule);
+        
+        Rule rule = new Rule("COVID_Regla_12");
+        rule.addAntecedent(new EqualsClause(Hechos.fiebre, "si"));
+        rule.addAntecedent(new EqualsClause(Hechos.dolorGarganta, "si"));
+        rule.addAntecedent(new EqualsClause(Hechos.diarrea, "si"));
+        rule.addAntecedent(new OrClause(
+                new EqualsClause(Hechos.perdidaOlfato, "si"),
+                new EqualsClause(Hechos.perdidaGusto, "si")
+        ));
+        rule.setConsequent(new EqualsClause(Hechos.COVID, "si"));
+        rie.addRule(rule);
+
+        Rule rule = new Rule("COVID_Regla_13");
+        rule.addAntecedent(new EqualsClause(Hechos.fiebre, "si"));
+        rule.addAntecedent(new EqualsClause(Hechos.dolorCabeza, "si"));
+        rule.addAntecedent(new EqualsClause(Hechos.fatiga, "si"));
+        rule.addAntecedent(new EqualsClause(Hechos.congestionNasal, "si"));
+        rule.setConsequent(new EqualsClause(Hechos.COVID, "si"));
+        rie.addRule(rule);
+
+        Rule rule = new Rule("COVID_Regla_14");
+        rule.addAntecedent(new EqualsClause(Hechos.contactoCOVID, "si"));
+        rule.addAntecedent(new EqualsClause(Hechos.fatiga, "si"));
+        rule.addAntecedent(new EqualsClause(Hechos.dolorGarganta, "si"));
+        rule.addAntecedent(new OrClause(
+                new EqualsClause(Hechos.perdidaOlfato, "si"),
+                new EqualsClause(Hechos.perdidaGusto, "si")
+        ));
+        rule.setConsequent(new EqualsClause(Hechos.COVID, "si"));
+        rie.addRule(rule);
+        
+        Rule rule = new Rule("COVID_Regla_15");
+        rule.addAntecedent(new EqualsClause(Hechos.contactoCOVID, "si"));
+        rule.addAntecedent(new EqualsClause(Hechos.fiebre, "si"));
+        rule.addAntecedent(new EqualsClause(Hechos.tos, "si"));
+        rule.addAntecedent(new EqualsClause(Hechos.escalofrios, "si"));
+        rule.setConsequent(new EqualsClause(Hechos.COVID, "si"));
+        rie.addRule(rule);
         
     }
 }
